a55943f943987630d9f6abe2e86aaf46a37123b7
Kande <man.kanm13@gmail.com>
2024-06-13 01:18:36 +0200

    mini-project navie bayes done

--- mini_project_navie_bayes/nb_author_id_mk.py
+++ mini_project_navie_bayes/nb_author_id_mk.py
@@ -8,7 +8,30 @@ sys.path.append(tools_path)
 
 from emails_Process_mk import preprocess_mk
 
-features_train_transformed, features_test_transformed, labels_train, labels_test = preprocess_mk()
+#Prétraiter les données
+features_train, features_test, labels_train, labels_test = preprocess_mk()
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Créer le classificateur
+clf = GaussianNB()
+
+#Entrainer le classificateur
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time() - t0, 3), "s")
+
+# Faire des predictions
+t1 = time()
+pred = clf.predict(features_test)
+print("Prediction Time:", round(time() - t1, 3), "s")
+
+# Calculer et afficher la prédiction
+accuracy = accuracy_score(labels_test, pred)
+print(f"Accuracy: {round(accuracy, 3)}")
+
+
 
 
 
